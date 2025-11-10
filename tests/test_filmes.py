bb591e9b164533f64b828052f109574925edc0a6
Sérgio Alves <alvessergio781@gmail.com>
2025-11-10 20:44:49 -0300

    fix(tests): corrige importação do app para execução no GitHub Actions

--- tests/test_filmes.py
+++ tests/test_filmes.py
@@ -1,4 +1,10 @@
 import json
+import sys
+import os
+import pytest
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
 
 def test_delete_filme_existente():
